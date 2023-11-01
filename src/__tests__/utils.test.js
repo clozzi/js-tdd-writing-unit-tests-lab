9b058c12a08d9b130cd5a40b61ec7720202b539e
Christopher Lozzi <clozzi12@gmail.com>
2023-11-01 11:56:31 -0400

    mvp completed

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,9 @@
-// Your tests here
+import { isPalindrome } from "../utils";
+
+describe("isPalindrome", () => {
+    it("gives users double points if their word is a palindrome (1 pt per vowel, 2 per consonant)", () => {
+        const word = "racecar";
+        const flipped = word.split('').reverse().join('');
+        expect(flipped).toBe(word)
+    })
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,8 @@
-// Your code here
+export function isPalindrome(word) {
+    let flipped = word.split('').reverse().join('');
+    if (flipped === word) {
+        return true;
+    } else {
+        return false
+    }
+}
